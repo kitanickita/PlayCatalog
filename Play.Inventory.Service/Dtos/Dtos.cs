26efa04635cd407fd2380d481d57fd3f6870a9ef
Kozlov Nikita <no.kozlov@northis.ru>
2021-05-12 16:55:41 +0300

    feature: Add catalog project to solution.

---
+++ Play.Inventory.Service/Dtos/Dtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Inventory.Service.Dtos
+{
+	public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+
+	public record InventoryItemDto(Guid CatalogItemId, string Name, string Description, int Quantity, DateTimeOffset AcquiredDate);
+
+	public record CatalogItemDto(Guid Id, string Name, string Description);
+}
